a1051923b84fcbaa25ca7f10911534fd0e3a8c12
Chris Carr and Megan Warnock <megan@>
2017-03-23 13:42:38 -0700

    add city route

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -11,12 +13,21 @@ public class App {
     City seattle = new City("Seattle");
     City spokane = new City("Spokane");
 
-    get("/", (response, request) -> {
+    get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
       model.put("template", "templates/index.vtl");
       model.put("cities", City.all());
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/cities/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      City city = City.find(Integer.parseInt(request.params(":id")));
+      model.put("city", city);
+      model.put("template", "templates/city.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 
   }
 }
