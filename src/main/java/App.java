75aee503d41b4a52535d097e27b716ab4f0cf18f
Chris Carr and Megan Warnock <megan@>
2017-03-23 11:46:48 -0700

    add user interface logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
@@ -6,6 +7,16 @@ import static spark.Spark.*;
 public class App {
   public static void main(String[] args) {
     String layout = "templates/layout.vtl";
+    City portland = new City("Portland");
+    City seattle = new City("Seattle");
+    City spokane = new City("Spokane");
+
+    get("/", (response, request) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      model.put("cities", City.all());
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
   }
 }
